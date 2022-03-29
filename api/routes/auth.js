0da1ef3a51053a00877bd75ab083a742b2d229f2
caosy <csy015676@163.com>
2022-03-29 13:46:49 +0800

    密码加密保护

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,15 +1,22 @@
 const router = require("express").Router();
-
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 //register
 router.post("/register",async (req,res)=>{
-    const newUser = await new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    });
+    
     try{
+        //将密码加密
+        const salt =  await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password,salt);
+
+        //创建新对象
+        const newUser = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashPassword,
+        });
+
         const user = await newUser.save();
         res.status(200).json(user);
     }catch(err){
