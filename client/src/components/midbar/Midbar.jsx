e1eab5ecdff94860fe70f8dbbe25d56aa1b4babf
caosy <csy015676@163.com>
2022-03-30 23:44:45 +0800

    帖子组件

--- client/src/components/midbar/Midbar.jsx
+++ client/src/components/midbar/Midbar.jsx
@@ -1,11 +1,13 @@
 import "./Midbar.css"
 import Share from "../share/Share"
+import Post from "../post/Post"
 
 export default function Midbar() {
   return (
     <div className="midbar">
         <div className="midbarWrapper">
           <Share/>
+          <Post/>
         </div>
     </div>
   )
---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,34 @@
+import "./Post.css"
+import { MoreVert } from "@mui/icons-material"
+export default function Post() {
+  return (
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
+                    <span className="postUsername">caosy</span>
+                    <span className="postDate">5 mins ago</span>
+                </div>
+                <div className="postTopRight">
+                    <MoreVert/>
+                </div>
+            </div>
+            <div className="postCenter">
+                <span className="postText">Hey,its my first post:)</span>
+                <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img src="/assets/like.png" alt="" className="likeIcon" />
+                    <img src="/assets/heart.png" alt="" className="likeIcon" />
+                    <span className="postLikeCounter">32 people like it</span>
+                </div>
+                <div className="postBottomRight">
+                    <span className="postCommentText">9 comments</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
