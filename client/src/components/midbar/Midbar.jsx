14d79b461dba95d14de609837059fa33f52cf9ba
caosy <csy015676@163.com>
2022-03-30 22:58:52 +0800

    share组件

--- client/src/components/midbar/Midbar.jsx
+++ client/src/components/midbar/Midbar.jsx
@@ -1,9 +1,12 @@
 import "./Midbar.css"
+import Share from "../share/Share"
 
 export default function Midbar() {
   return (
     <div className="midbar">
-        Midbar
+        <div className="midbarWrapper">
+          <Share/>
+        </div>
     </div>
   )
 }
